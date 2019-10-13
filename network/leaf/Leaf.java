1211c64b9c989c5c77c7dd8c4afae0341910c433
Ahmed Sakr <ahmed@sakr.ca>
2019-10-13 12:36:29 -0400

    Develop leaf to register with CPS

--- network/leaf/Leaf.java
+++ network/leaf/Leaf.java
@@ -9,35 +9,145 @@ import network.core.Packet;
 import network.core.Transport;
 import network.core.exceptions.CorruptPacketException;
 import network.core.packets.LeafRegistration;
+import network.core.packets.RegistrationResponse;
 
 /**
- * A Leaf is a network node 
+ * A Leaf is a network endpoint that is a point of interest
+ * for the SmartGrow system.
+ * 
+ * A leaf could be:
+ * - A plant endpoint
+ *      A node which provides sensory information on a plant
+ * - An android user
+ *      An application that interacts with the SmartGrow system
+ *      by providing input and receiving sensory information.
+ * 
+ * Using this logical grouping, the Leaf abstraction completely
+ * abstracts the UDP networking logic by automatically registering
+ * with the central processing server and communicating with its
+ * assigned branch.
+ * 
+ * @author Ahmed Sakr
+ * @since October 10, 2019
  */
 public class Leaf extends Transport {
 
+    // Synchronizing access to the socket and state variables
+    private Object lock;
+
+    // State information about this leaf instance
     private Identity identity;
+    private boolean registered;
+
+    // The assigned branch's networking address and port
+    private String branchAddress;
+    private int branchPort;
 
     /**
-     * Initialize a leaf node listening on the provided port.
+     * Checks if the leaf has registered with the central processing server.
      * 
-     * @param port The port to listen on
+     * @return  true    If the leaf has established a connection with the central
+     *                  processing server
+     *          false   Otherwise
+     */
+    public boolean isRegistered() {
+        synchronized (this.lock) {
+            return this.registered;
+        }
+    }
+
+    /**
+     * Block sending until registration with the central processing server is
+     * complete.
+     * 
+     * @param packet The packet to send to the branch
+     */
+    @Override
+    public void send(Packet packet) throws IOException {
+        this.waitForRegistration();
+
+        // Override the destination address and port to the branch assigned to this leaf.
+        packet.setDestination(this.branchAddress, this.branchPort);
+
+        super.send(packet);
+    }
+
+    /**
+     * Block receiving until registration with the central processing server is
+     * complete.
+     * 
+     * @return The verified packet received from the branch.
+     */
+    @Override
+    public Packet receive() throws CorruptPacketException, IOException {
+        this.waitForRegistration();
+
+        return super.receive();
+    }
+
+    /**
+     * Create a Leaf with a provided identity.
+     * 
+     * @param identity The identity of this leaf (Android user or Plant endpoint)
      */
     public Leaf(Identity identity) throws SocketException {
         super(new Random().nextInt(0xFFFF));
         this.identity = identity;
+        this.lock = new Object();
 
         this.register();
     }
 
-    private void register() {
-        try {
-            LeafRegistration registration = new LeafRegistration();
-            registration.setIdentity(this.identity);
-            registration.setDestination(Configuration.CPS_ADDRESS, Configuration.CPS_PORT);
-            this.send(registration);
-            Packet response = this.receive();
-        } catch (IOException | CorruptPacketException ex) {
-            System.err.printf("CRITICAL: failed to register with CPS: %s\n", ex);
+
+    /*
+     * Join the lock wait set if registration has still not completed. 
+     */
+    private void waitForRegistration() {
+        synchronized (this.lock) {
+            if (!this.registered) {
+                try {
+
+                    // Once we exit the wait set, it is assumed that registration is now
+                    // complete.
+                    // TODO: Check if registration failed?
+                    this.lock.wait();
+                } catch (InterruptedException ex) {
+                    System.err.println("CRITICAL: interrupted while waiting for leaf registration");
+                }
+            }
         }
+    }
+
+    /*
+     * Spawn a thread to register this leaf with the central processing server. 
+     */
+    private void register() {
+        new Thread(() -> {
+            try {
+                LeafRegistration registration = new LeafRegistration();
+                registration.setIdentity(this.identity);
+                registration.setDestination(Configuration.CPS_ADDRESS, Configuration.CPS_PORT);
+
+                synchronized (this.lock) {
+                    super.send(registration);
+                    RegistrationResponse response = (RegistrationResponse) super.receive();
+
+                    if (response.getStatus()) {
+                        this.registered = true;
+                    } else {
+
+                        // TODO: how to recover from a failure to register with the server?
+                        this.registered = false;
+                    }
+                    
+                    this.branchAddress = response.getAddress();
+                    this.branchPort = response.getPort();
+                    this.lock.notifyAll();
+                }
+
+            } catch (IOException | CorruptPacketException ex) {
+                System.err.printf("CRITICAL: failed to register with CPS: %s\n", ex);
+            }
+        }).start();
     }
 }
