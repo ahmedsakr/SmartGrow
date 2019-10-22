6b0c39f2c1f3604517b2927a737c1497edd6591c
Ahmed Sakr <ahmed@sakr.ca>
2019-10-22 12:20:34 -0400

    Modify CPS address to the static IP address

--- network/Configuration.java
+++ network/Configuration.java
@@ -2,6 +2,6 @@ package network;
 
 public class Configuration {
 
-    public static final String CPS_ADDRESS = "127.0.0.1";
+    public static final String CPS_ADDRESS = "10.0.0.21";
     public static final int CPS_PORT = 3010;
 }
