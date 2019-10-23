48085d155b5c8a21584edd8a0fca10be10552648
Ashfaqul-007 <ashfaqul.haque@outlook.com>
2019-10-23 15:47:49 -0400

    Sensor code finished

--- endpoint/PhotoresistorLED/PhotoresistorLED.ino
+++ endpoint/PhotoresistorLED/PhotoresistorLED.ino
@@ -1,34 +1,15 @@
-const int PRPin = A0;
-const int LEDPin = 13;
+const int PRPin = A1;
 
 int PRValue = 0;
-int LEDValue = 0;
-int LEDBrightness = 0;
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
   pinMode(PRPin, INPUT);
-  pinMode(LEDPin, OUTPUT);
 }
 
 void loop() {
   PRValue = analogRead(PRPin);
   Serial.print(PRValue);
-  Serial.print("\t");
-  LEDValue = getLEDValue(PRValue);
-  Serial.print(LEDValue);
   Serial.print("\n");
-//  LEDBrightness = getLEDBrightness(LEDValue);
-  analogWrite(LEDPin, LEDValue);
-//  Serial.print("The LEB is operating at " + LEDBrightnes + "%.\n");
   delay(30);
 }
-
-int getLEDValue(int PRInput){
-  double a = PRInput / 1023.0 * 225.0;
-  return (int)a;
-}
-
-//int getLEDBrightness(int LEDValue){
-//  return (int)(LEDValue * (100/255));
-//}
--- endpoint/SensorData/SensorData.ino
+++ endpoint/SensorData/SensorData.ino
@@ -1,20 +1,20 @@
-#include <dht.h>
-#define DELAY 10000
+#include "dht11.h"
+#define DELAY 1000
 
-dht DHT;
+dht11 DHT;
 const int SMSensorPin = A0;
 const int PRPin = A1;
 const int LEDPin = 13;
 const int DHTPin = 4;
 
 int SMSensorValue = 0;
-int SMPercent = 0;
+double SMPercent = 0;
 int PRValue = 0;
-int LEDValue = 0;
-int LEDBrightness = 0;
+double lux = 0, ADCValue = 0.0048828125;
+int DTHValue;
 
 void setup() {
-  Serial.begin(115200);
+  Serial.begin(9600);
   pinMode(SMSensorPin, INPUT);
   pinMode(PRPin, INPUT);
   pinMode(LEDPin, OUTPUT);
@@ -24,52 +24,41 @@ void loop() {
   
   SMSensorValue = analogRead(SMSensorPin); //get soil moisture value
   PRValue = analogRead(PRPin); //get photo resistor value
-  DTHValue = DHT.read11(DHTPin); //get DHT sensor value
+  DTHValue = DHT.read(DHTPin); //get DHT sensor value
   switch (DTHValue)
   {
     case DHTLIB_OK:  
-                Serial.print("OK,\t"); 
                 break;
     case DHTLIB_ERROR_CHECKSUM: 
-                Serial.print("Checksum error,\t"); 
                 break;
     case DHTLIB_ERROR_TIMEOUT: 
-                Serial.print("Time out error,\t"); 
                 break;
     default: 
-                Serial.print("Unknown error,\t"); 
                 break;
   }
   SMPercent = SMConvertToPercentage(SMSensorValue);
-  LEDValue = getLEDValue(PRValue);
-  LEDBrightness = getLEDBrightness(LEDValue);
-  analogWrite(LEDPin, LEDValue); //set LED brightness to the corresponding value
+  lux = getLux(PRValue);
   
-  Serial.print(SMPercent);
+  Serial.print(SMSensorValue);
   Serial.print("\t");
-  Serial.print(LEDBrightness);
+  Serial.print(lux);
   Serial.print("\t");
   Serial.print(DHT.humidity,1);
   Serial.print("\t");
   Serial.print(DHT.temperature,1);
   Serial.print("\t");
+  Serial.print("\n");
 
   delay(DELAY);
 }
 
 //returns the soil moisture percentge given the analog input from the soil moisture sensor
-int SMConvertToPercentage(int value){
-  int percentage = 0;
-  percentage = value * (100/1023);
+double SMConvertToPercentage(int value){
+  double percentage = 0;
+  percentage = (double)value * (100.0/1023.0);
   return percentage;
 }
 
-//returns the corresponding LED brightness value given the input from the photoresistor
-int getLEDValue(int PRInput){
-  return (int)(PRInput * (255/1023));
-}
-
-//returns the LED brightness percentage given the LED brightness value
-int getLEDBrightness(int LEDValue){
-  return (int)(LEDValue * (100/255));
+double getLux(int PRValue){
+  return (double)(250.0 / (ADCValue * (float)PRValue)) - 50.0;
 }
