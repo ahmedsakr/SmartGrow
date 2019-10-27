056efa270d7504ee7c2013ecbf9da8f1bce180a1
Ahmed Sakr <ahmed@sakr.ca>
2019-10-27 13:55:12 -0400

    Prune leaf servicers when a leaf has not communicated in a long time

---
+++ network/branch/threads/LeafPruningThread.java
@@ -0,0 +1,80 @@
+package network.branch.threads;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import network.branch.Branch;
+import network.branch.DedicatedLeafServicer;
+
+/**
+ * LeafPruningThread is spawned when a branch is created to
+ * clean up leaves that have stopped communicating.
+ * 
+ * @author Ahmed Sakr
+ * @since October 27, 2019
+ */
+public class LeafPruningThread extends Thread {
+
+    private static Logger logger = LogManager.getLogger(LeafPruningThread.class);
+
+    // The period at which this thread kicks off to check for pruning validity.
+    public static final int LEAF_PRUNING_INTERVAL = 2500;
+
+    // The time that elapses after no packet has been received when pruning should kick in.
+    public static final int LEAF_PRUNING_THRESHOLD = 5000;
+
+    // The branch that we are pruning dead leaves from.
+    private Branch branch;
+
+    public LeafPruningThread(Branch branch) {
+        super(String.format("%sBranch-PruningThread", branch));
+        this.branch = branch;
+
+        // Immediately start the pruning thread.
+        this.start();
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+
+            ArrayList<DedicatedLeafServicer> servicers = null;
+
+            try {
+
+                // Sleep the interval time specified for pruning
+                Thread.sleep(LEAF_PRUNING_INTERVAL);
+                
+                // This whole operation is a critical section.
+                // No one should be able to modify the list of servicers until
+                // we are done.
+                synchronized (this.branch) {
+                    servicers = this.branch.getServicers();
+                    long currentTime = System.currentTimeMillis();
+
+                    // Check and collect all servicers whose leaf has not communicated past the
+                    // acceptable threshold.
+                    List<DedicatedLeafServicer> deadLeaves = 
+                        servicers.stream()
+                            .filter((servicer) -> currentTime - servicer.getLastReceivedTime() >= LEAF_PRUNING_THRESHOLD)
+                            .collect(Collectors.toList());
+
+
+                    for (DedicatedLeafServicer servicer : deadLeaves) {
+                        logger.info("Stopping servicer for " + servicer.getDestination());
+
+                        // Interrupt and close the transport layer for the servicer.
+                        servicer.stop();
+                        servicers.remove(servicer);
+                    }
+                }
+            } catch (InterruptedException ex) {
+                logger.error("Interrupted while sleeping. Pruning for branch " + this.branch + " is disabled.");
+            }
+        }
+    }
+}
