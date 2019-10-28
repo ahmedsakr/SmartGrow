f7fa2f2ee3b6d32e2fec5f10cf8b656fc45d3992
Valerie Figuracion <47678410+vmfiguracion@users.noreply.github.com>
2019-10-28 01:54:28 -0400

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -3,7 +3,13 @@ package com.example.smartgrow20;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
+import endpoint.sensors.SupportedSensors;
+import java.io.IOException;
+
+import network.core.Packet;
+import network.core.exceptions.CorruptPacketException;
 import network.core.packets.SensorsData;
+import network.leaf.Leaf;
 
 import static endpoint.sensors.SupportedSensors.AIR_HUMIDITY;
 import static endpoint.sensors.SupportedSensors.AIR_TEMPERATURE;
@@ -18,6 +24,7 @@ import static endpoint.sensors.SupportedSensors.SOIL_MOISTURE;
 
 public class MainActivity extends AppCompatActivity {
 
+    public Leaf leaf;
     public SensorsData sensors;
 
     @Override
@@ -25,22 +32,31 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        double light_data = sensors.getSensorData(LIGHT_INTENSITY);
-        double soil_data = sensors.getSensorData(SOIL_MOISTURE);
-        double temperature_data = sensors.getSensorData(AIR_TEMPERATURE);
-        double humidity_data = sensors.getSensorData(AIR_HUMIDITY);
+        try{
+            Packet pkt = leaf.receive();
+            SensorsData sd = ((SensorsData) pkt);
+            
+            //sensors.addSensorData(LIGHT_INTENSITY, ?? );
+            //sensors.addSensorData(SOIL_MOISTURE, soil_data);
+            //sensors.addSensorData(AIR_TEMPERATURE, temperature_data);
+            //sensors.addSensorData(AIR_HUMIDITY, humidity_data);
 
-        //The following corresponds with the strings.xml file
-        String light = getString(R.string.lightIntensity, light_data);
-        String soil = getString(R.string.soilMoisture, soil_data);
-        String temp = getString(R.string.airTemperature, temperature_data);
-        String humidity = getString(R.string.airHumidity, humidity_data);
+            //Gets the values from the hash map using the sensorID as the key.
+            double light_data = sensors.getSensorData(LIGHT_INTENSITY);
+            double soil_data = sensors.getSensorData(SOIL_MOISTURE);
+            double temperature_data = sensors.getSensorData(AIR_TEMPERATURE);
+            double humidity_data = sensors.getSensorData(AIR_HUMIDITY);
 
+            //The following corresponds with the strings.xml file
+            String light = getString(R.string.lightIntensity, light_data);
+            String soil = getString(R.string.soilMoisture, soil_data);
+            String temp = getString(R.string.airTemperature, temperature_data);
+            String humidity = getString(R.string.airHumidity, humidity_data);
 
-
-
-
-
-
+        } catch(CorruptPacketException e){
+            //log.error(e);
+        } catch(IOException e){
+            //log.error(e);
+        }
     }
 }
