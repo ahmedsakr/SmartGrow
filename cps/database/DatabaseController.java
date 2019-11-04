c98199310283c3cc0e90aa7e8ebbc8fd3607b622
Ahmed Sakr <ahmed@sakr.ca>
2019-11-04 08:09:15 -0500

    Add methods for querying and updating database (#16)

--- cps/database/DatabaseController.java
+++ cps/database/DatabaseController.java
@@ -2,7 +2,9 @@ package cps.database;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Properties;
 
 import org.apache.logging.log4j.LogManager;
@@ -36,8 +38,45 @@ public class DatabaseController {
     public DatabaseController() throws SmartgrowDatabaseException {
 
         // Establish connection with the Postgres database.
-        this.connectToDatabase();
+        this.connect();
+
         logger.debug("Established database connection");
+    }
+
+    /**
+     * Query the database with the provided SQL prepared statement.
+     *
+     * @param sql The prepared statement
+     * @return The result of the query
+     */
+    public ResultSet query(String sql) throws SmartgrowDatabaseException {
+        try {
+
+            // Execute the result-returning query.
+            Statement statement = this.dbConnection.createStatement();
+            return statement.executeQuery(sql);
+
+        } catch (SQLException ex) {
+            throw new SmartgrowDatabaseException(ex.getMessage());
+        }
+    }
+
+    /**
+     * Query the database with an INSERT, UPDATE, or DELETE (i.e., statements
+     * that do not return a result).
+     *
+     * @param sql The prepared statement
+     */
+    public void update(String sql) throws SmartgrowDatabaseException {
+        try {
+
+            // Execute the no-result update query.
+            Statement statement = this.dbConnection.createStatement();
+            statement.executeUpdate(sql);
+
+        } catch (SQLException ex) {
+            throw new SmartgrowDatabaseException(ex.getMessage());
+        }
     }
 
     /**
@@ -47,13 +86,7 @@ public class DatabaseController {
      * @throws SmartgrowDatabaseException
      * @see {@link cps.database.DatabaseInfo}
      */
-    private void connectToDatabase() throws SmartgrowDatabaseException {
-
-        // Specify the database user, password, and SSL requirement for the connection.
-        Properties dbCredentials = new Properties();
-        dbCredentials.setProperty("user", DatabaseInfo.DATABASE_USER);
-        dbCredentials.setProperty("password", System.getenv(DatabaseInfo.DATABASE_PASSWORD));
-        dbCredentials.setProperty("ssl", "false");
+    private void connect() throws SmartgrowDatabaseException {
 
         // Attempt to retrieve the dynamic database user password from the environment.
         String user_password = System.getenv(DatabaseInfo.DATABASE_PASSWORD);
@@ -62,6 +95,12 @@ public class DatabaseController {
             // The Smartgrow database user password variable is not set in the system.
             throw new SmartgrowDatabaseException("Database user environment variable not set");
         }
+
+        // Specify the database user, password, and SSL requirement for the connection.
+        Properties dbCredentials = new Properties();
+        dbCredentials.setProperty("user", DatabaseInfo.DATABASE_USER);
+        dbCredentials.setProperty("password", user_password);
+        dbCredentials.setProperty("ssl", "false");
 
         try {
             this.dbConnection = DriverManager.getConnection(DatabaseInfo.DATABASE_URL, dbCredentials);
---
+++ cps/database/schemas/plants.sql
@@ -0,0 +1,12 @@
+
+CREATE DATABASE smartgrow;
+GRANT ALL PRIVILEGES ON DATABASE smartgrow TO smartgrow_client;
+\c smartgrow;
+
+CREATE TABLE recommended_properties(
+    plant_name VARCHAR(32) PRIMARY KEY,
+    light_intensity NUMERIC,
+    air_humidity NUMERIC,
+    air_temperature NUMERIC,
+    soil_moisture NUMERIC
+);
