a490f2e1afe23962398394db49b0fabb7b985e15
vmfiguracion <valeriemae.figuracion@gmail.com>
2019-11-09 16:38:07 -0500

    Updated MainActivity for easier reading

--- application/src/main/java/com/example/sg/MainActivity.java
+++ application/src/main/java/com/example/sg/MainActivity.java
@@ -12,9 +12,9 @@ import java.net.SocketException;
 
 import network.core.Packet;
 import network.core.exceptions.CorruptPacketException;
-import network.core.packets.SensorsData;
 import network.leaf.Identity;
 import network.leaf.Leaf;
+import network.core.packets.sensors.*;
 
 /**
  * Displays the sensors from the plant endpoints.
@@ -25,13 +25,25 @@ import network.leaf.Leaf;
 public class MainActivity extends AppCompatActivity {
 
     public Leaf leaf;
-    //public SensorsData sensors;
+    public RequestSensors requestSensors;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        requestInfo();
+        sensorInfo();
+    }
+
+    protected void requestInfo(){
+        new Thread(() -> requestSensors = new RequestSensors()).start();
+    }
+
+    protected void sensorInfo(){
+
+        //Initialize a leaf for the user.
         try {
             leaf = new Leaf(Identity.ANDROID_USER);
         } catch (SocketException e) {
@@ -39,32 +51,27 @@ public class MainActivity extends AppCompatActivity {
         }
 
         try {
+            //Initialize a leaf for receiving the sensor information.
             Packet pkt = leaf.receive();
             SensorsData sd = ((SensorsData) pkt);
 
-            double lightData = sd.getSensorData(SupportedSensors.LIGHT_INTENSITY);
-            double soilData = sd.getSensorData(SupportedSensors.SOIL_MOISTURE);
-            double temperatureData = sd.getSensorData(SupportedSensors.AIR_TEMPERATURE);
-            double humidityData = sd.getSensorData(SupportedSensors.AIR_HUMIDITY);
-
+            //Obtains the area where the data will be shown.
             TextView light_Data = findViewById(R.id.light_data);
             TextView soil_Data = findViewById(R.id.soil_data);
             TextView temperature_Data = findViewById(R.id.temperature_data);
             TextView humidity_Data = findViewById(R.id.humidity_data);
 
-            String LD = Double.toString(lightData);
-            String SD = Double.toString(soilData);
-            String TD = Double.toString(temperatureData);
-            String HD = Double.toString(humidityData);
+            String light_reading = "Current reading: " + sd.getSensorData(SupportedSensors.LIGHT_INTENSITY);
+            String soil_reading = "Current reading: " + sd.getSensorData(SupportedSensors.SOIL_MOISTURE);
+            String temp_reading = "Current reading: " + sd.getSensorData(SupportedSensors.AIR_TEMPERATURE);
+            String humid_reading = "Current reading: " + sd.getSensorData(SupportedSensors.AIR_HUMIDITY);
 
-            light_Data.setText("Current reading: " + LD);
-            soil_Data.setText("Current reading: " + SD);
-            temperature_Data.setText("Current reading: " + TD);
-            humidity_Data.setText("Current reading: " + HD);
+            light_Data.setText(light_reading);
+            soil_Data.setText(soil_reading);
+            temperature_Data.setText(temp_reading);
+            humidity_Data.setText(humid_reading);
 
-        } catch (CorruptPacketException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
+        } catch (CorruptPacketException | IOException e) {
             e.printStackTrace();
         }
     }
