e27abef9ea4307387fb34b158c99f4017a9cc0d9
Valerie Figuracion <47678410+vmfiguracion@users.noreply.github.com>
2019-11-19 22:14:43 -0500

    Updates files for the photoresistor (#33)

--- endpoint/PhotoresistorLED/StubPhotoresistorLED.ino
+++ endpoint/PhotoresistorLED/StubPhotoresistorLED.ino
@@ -1,18 +1,15 @@
+#include <stdlib.h>
 #define DELAY 1000
-
-//Declare sensors
-const int PRPin = A1; 
 
 int PRValue = 0;
 const double ADCValue = 0.0048828125; //Analog to Digital Converter value
 
 void setup() {
-    Serial.begin(115200); //baud rate
-    pinMode(PRPin, INPUT); //initialize the PRPin to be input 
+    Serial.begin(115200); //baud rate 
 }
 
 void loop() {
-    PRValue = analogRead(PRPin);
+    PRValue = (rand() %500) //Simulates the input from the photoresistor
     Serial.println(getLux(PRValue));
     delay(DELAY);//delay for 1 second
 }   
