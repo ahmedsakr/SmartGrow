de0fa77ccb1a40a33b915ab1b8c092b8e5824b4b
Ahmed Sakr <ahmed@sakr.ca>
2019-11-24 11:14:34 -0500

    Fix bug where rotating phone causes new leaf creation (#40)

--- application/app/src/main/java/com/example/smartgrow/MainActivity.java
+++ application/app/src/main/java/com/example/smartgrow/MainActivity.java
@@ -5,6 +5,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+/**
+ * MainActivity is the entry activity when the user opens
+ * the SmartGrow android application.
+ *
+ * Currently, the Activity shows the real-time sensor information
+ * retrieved from the server.
+ *
+ * @author Ahmed Sakr
+ * @since November 23, 2019
+ */
 public class MainActivity extends AppCompatActivity {
 
     private RealtimeSensors realtimeSensors;
@@ -14,8 +24,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
         this.realtimeSensors = new RealtimeSensors(this);
+    }
 
+    /**
+     * When destroying the activity, we must stop the thread that
+     * is updating the sensor information.
+     */
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        // End the thread for retrieving the sensor information from the server.
+        this.realtimeSensors.interrupt();
     }
 }
--- application/app/src/main/java/com/example/smartgrow/RealtimeSensors.java
+++ application/app/src/main/java/com/example/smartgrow/RealtimeSensors.java
@@ -7,6 +7,7 @@ import java.net.SocketException;
 import java.util.Locale;
 
 import endpoint.sensors.SupportedSensors;
+import logging.SmartLog;
 import network.core.exceptions.CorruptPacketException;
 import network.core.packets.sensors.RequestSensors;
 import network.core.packets.sensors.SensorsData;
@@ -14,12 +15,17 @@ import network.leaf.Identity;
 import network.leaf.Leaf;
 
 /**
- * RealtimeSensors provides
+ * RealtimeSensors is responsible for establishing a connection with the server
+ * and retrieving the sensors that are displayed on the application.
+ *
+ * @author Ahmed Sakr
+ * @since November 23, 2019
  */
 public class RealtimeSensors extends Thread {
 
+    private SmartLog logger = new SmartLog(RealtimeSensors.class.getName());
+
     private MainActivity activity;
-    private Leaf leaf;
 
     public RealtimeSensors(MainActivity activity) {
         super("RealtimeSensors");
@@ -29,10 +35,17 @@ public class RealtimeSensors extends Thread {
 
     @Override
     public void run() {
+
+        // Initialize a connection in the SmartGrow system.
+        Leaf leaf;
+
         try {
-            this.leaf = new Leaf(Identity.ANDROID_USER);
+            leaf = new Leaf(Identity.ANDROID_USER);
         } catch (SocketException ex) {
-            ex.printStackTrace();
+
+            // Prematurely terminate if we can't get hold of a leaf object.
+            logger.fatal("Unable to initialize the SmartGrow leaf: " + ex.getMessage());
+            return;
         }
 
 
@@ -41,26 +54,39 @@ public class RealtimeSensors extends Thread {
         TextView airHumidity = activity.findViewById(R.id.textView4);
         TextView lightIntensity = activity.findViewById(R.id.textView6);
         TextView soilMoisture = activity.findViewById(R.id.textView8);
-        while (true) {
-            try {
+        try {
+            while (true) {
 
-                this.leaf.send(request);
+                // Send the request for fresh sensor data to the server.
+                leaf.send(request);
 
-                SensorsData data = (SensorsData) this.leaf.receive();
+                // Wait for the response from the server.
+                SensorsData data = (SensorsData) leaf.receive();
 
+                // Update the sensor information on the screen
                 activity.runOnUiThread(() -> {
                     temperature.setText(String.format(Locale.CANADA, "%.1f", data.getSensorData(SupportedSensors.AIR_TEMPERATURE)));
-                    airHumidity.setText(String.format(Locale.CANADA,"%.1f", data.getSensorData(SupportedSensors.AIR_HUMIDITY)));
-                    lightIntensity.setText(String.format(Locale.CANADA,"%.1f", data.getSensorData(SupportedSensors.LIGHT_INTENSITY)));
-                    soilMoisture.setText(String.format(Locale.CANADA,"%.1f", data.getSensorData(SupportedSensors.SOIL_MOISTURE)));
+                    airHumidity.setText(String.format(Locale.CANADA, "%.1f", data.getSensorData(SupportedSensors.AIR_HUMIDITY)));
+                    lightIntensity.setText(String.format(Locale.CANADA, "%.1f", data.getSensorData(SupportedSensors.LIGHT_INTENSITY)));
+                    soilMoisture.setText(String.format(Locale.CANADA, "%.1f", data.getSensorData(SupportedSensors.SOIL_MOISTURE)));
                 });
 
+                // Sleep for a second before requesting another fresh batch of sensors.
                 Thread.sleep(1000);
-
-            } catch (IOException | InterruptedException | CorruptPacketException ex) {
-                ex.printStackTrace();
             }
-        }
+        } catch (IOException | CorruptPacketException ex) {
 
+            // Inability to perform network operations or receiving incorrect packets is
+            // grounds for exiting this thread.
+            logger.error("Encountered exception during operation: " + ex.getMessage());
+        } catch (InterruptedException ex) {
+
+            // Interruption is our signal to close this thread.
+            logger.debug("Exiting the RealtimeSensors due to normal interruption.");
+        } finally {
+
+            // Close the leaf before exiting the thread to relinquish the network resource.
+            leaf.close();
+        }
     }
 }
