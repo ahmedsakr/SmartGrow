4d09c9a1a870038d37f78b43982a6e71cb2ac77e
vmfiguracion <valeriemae.figuracion@gmail.com>
2019-12-02 01:13:44 -0500

    Updated serial files

--- endpoint/datacollection/SensorData/SensorData.ino
+++ endpoint/datacollection/SensorData/SensorData.ino
@@ -1,6 +1,3 @@
-#include <SPI.h>
-#include <Ethernet.h>
-#include <EthernetUdp.h>
 #include "dht11.h"
 #define DELAY 5000 //5 seconds
 
@@ -17,23 +14,12 @@ int DHTValue = 0;
 int SMSensorValue = 0;
 int PRValue = 0;
 
-//mac address for ehternet hardware shield
-byte mac[] = {0x9C, 0xB6, 0xD0, 0x0B, 0x1C, 0x41};
-IPAddress ip(172,17,95,249);
-
-unsigned int localPort = 8888;
-EthernetUDP Udp;
-
 unsigned int sizeOfMsg = 512;
 byte msg[512];
 
 void setup() {
     //Start listening to arduino
     Serial.begin(9600);
-
-    //Start the ethernet & UDP
-    Ethernet.begin(mac, ip);
-    Udp.begin(localPort);
   
     //Configure inputs
     pinMode(SMSensorPin, INPUT);
@@ -42,27 +28,19 @@ void setup() {
     msg[0] = 3; //OPCODE 3 for SensorData   
 }
 
-void loop() {
-    Udp.beginPacket(Udp.remoteIP(), Udp.remotePort());
-    
+void loop() {   
+      
     lightSource();
     moistureContent();
     dhtValues();
-    /*
+    
     for (int i = 0; i < sizeof(msg); i++){
       Serial.print(msg[i]);
     }
-    */
-     
-    
-    //Writes UDP data to remote connection
-    Udp.write(msg, sizeOfMsg);
-    Udp.endPacket();
-    
     delay(DELAY);
 }
 
-/*separate each sensor into its own functions
+/*Separate each sensor into its own functions
  * 
  */
 void lightSource(){
@@ -74,9 +52,7 @@ void lightSource(){
     byte lightDataToByte = byte(lux);
 
     msg[1] = 1;
-    msg[2] = lightDataToByte;
-    
-    Serial.println(lightDataToByte);
+    msg[2] = lightDataToByte;    
 
 //Code to print information to the serial monitor
 
